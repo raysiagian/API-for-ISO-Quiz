64d402e3368d745aebc7ef7826c420af2cc0f600
raysiagian <anggasiagian17@gmail.com>
2024-11-20 16:11:24 +0700

    login, register, check email user

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\RegisterUserRequest;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+class UserController extends Controller
+{
+
+    public function register(RegisterUserRequest $request)
+    {
+        $request->validated();
+
+        $userData = [
+            'username' => $request->username,
+            'phoneNumber' => $request->phoneNumber,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ];
+
+        $user = User::create($userData);
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response([
+            'user'=>$user,
+            'token'=> $token,
+        ],200);
+    }
+
+    public function checkEmailAvailability(Request $request)
+    {
+        // Validate request data
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255',
+        ]);
+
+        // Check if validation fails
+        if ($validator->fails()) {
+            throw ValidationException::withMessages($validator->errors()->toArray());
+        }
+
+        // Check if email already exists
+        $user = User::where('email', $request->email)->first();
+
+        // Return response based on email availability
+        if ($user) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Email sudah terdaftar',
+            ]);
+        } else {
+            return response()->json([
+                'status' => true,
+                'message' => 'Email tersedia',
+            ]);
+        }
+    }
+    
+    public function login(LoginUserRequest $request)
+    {
+        $validatedData = $request->validated();
+
+        $user = User::where('email', $validatedData['email'])->first();
+
+        if (!$user || !Hash::check($validatedData['password'], $user->password)) {
+            return response()->json([
+                'message' => 'Email atau password anda salah'
+            ], 400);
+        }
+
+        if ($user->is_Active != 1) {
+            return response()->json([
+                'message' => 'Akun anda tidak aktif'
+            ], 400);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+            'is_Active' => $user->is_Active,
+        ], 200);
+    }
+
+
+     public function profile(Request $request)
+     {
+         $user = $request->user();
+ 
+         return response()->json([
+            'status' => true,
+            'message' => 'Profil pengguna',
+            'user' => $user,
+         ]);
+     }
+ 
+     // Logout API
+     public function logout(Request $request)
+     {
+         $request->user()->currentAccessToken()->delete();
+ 
+         return response()->json([
+            'status' => true,
+            'message' => 'Logout berhasil',
+         ]);
+     }
+ 
+     public function user(Request $request)
+     {
+        return response()->json($request->user());
+     }
+
+}
---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// User
+
+// Login dan Register User
+Route::post('register', [UserController::class,'register']);
+Route::post('login', [UserController::class,'login']);
+
+// Check ketersediaan email
+Route::post('check-email', [UserController::class, 'checkEmailAvailability']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
